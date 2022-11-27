686dc97bc702669b611de740bbdfd6c468d29121
Floppi87 <36642656+Floppi87@users.noreply.github.com>
2022-11-27 19:13:05 +0100

    Added DaisyUI and formular prototype

---
+++ software/messe-frontend/src/app/components/formular/formular.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'its-formular',
+  templateUrl: './formular.component.html',
+  styleUrls: ['./formular.component.css']
+})
+export class FormularComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
