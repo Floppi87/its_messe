0ca6017f1c16d77197f43e3d48ead5f5e9cf937c
Floppi87 <36642656+Floppi87@users.noreply.github.com>
2023-01-06 13:52:21 +0100

    Moved stuff around and deleted all the unneccessary stuff i put effort in

--- software/messe-frontend/src/app/app.module.ts
+++ software/messe-frontend/src/app/app.module.ts
@@ -6,18 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormularComponent } from './components/base/formular/formular.component';
 import { MainComponent } from './components/base/main/main.component';
-import { RequiredStarComponent } from './components/formular/required-star/required-star.component';
+import { RequiredStarComponent } from './components/base/formular/required-star/required-star.component';
 import { WebcamModule } from 'ngx-webcam';
-import { WebcamComponent } from './components/formular/webcam/webcam.component';
+import { WebcamComponent } from './components/base/formular/webcam/webcam.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { PersonalFormComponent } from './components/formular/personal-form/personal-form.component';
-import { PersonalAdressFormComponent } from './components/formular/personal-adress-form/personal-adress-form.component';
-import { WebcamFormComponent } from './components/formular/webcam-form/webcam-form.component';
-import { CompanyFormComponent } from './components/formular/company-form/company-form.component';
-import { InterestFormComponent } from './components/formular/interest-form/interest-form.component';
-import { FormControlComponent } from './components/formular/form-control/form-control.component';
-import { FinishedFormComponent } from './components/formular/finished-form/finished-form.component'
+import { WebcamFormComponent } from './components/base/formular/webcam-form/webcam-form.component';
+import { InterestFormComponent } from './components/base/formular/interest-form/interest-form.component';
 
 @NgModule({
   declarations: [
@@ -26,13 +21,8 @@ import { FinishedFormComponent } from './components/formular/finished-form/finis
     MainComponent,
     RequiredStarComponent,
     WebcamComponent,
-    PersonalFormComponent,
-    PersonalAdressFormComponent,
     WebcamFormComponent,
-    CompanyFormComponent,
-    InterestFormComponent,
-    FormControlComponent,
-    FinishedFormComponent
+    InterestFormComponent
   ],
   imports: [
     BrowserModule,
